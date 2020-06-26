90c422b42d7050aa3effffb99961051426461235
hieuhuynh12 <66456765+hieuhuynh12@users.noreply.github.com>
2020-06-26 11:00:18 +0700

    first commit

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_SERVER', 'ec2-34-200-72-77.compute-1.amazonaws.com');
-define('DB_USERNAME', 'bjcmcdrnkumbvz');
-define('DB_PASSWORD', '78db208b170ce33a0b1f40501400ca03318a38331c9df70d4469ffe534f65939');
-define('DB_NAME', 'dc9tmharsdf08g');
+define('DB_SERVER', 'ec2-52-87-135-240.compute-1.amazonaws.com');
+define('DB_USERNAME', 'zxrphqejgdjlbz');
+define('DB_PASSWORD', '7b2547c34fa2cef8435147f78174460c17c37a241f240c2acac7b72a9d37c837');
+define('DB_NAME', 'dc89t3rs7aj0p9');
  
 /* Attempt to connect to PostgreSQL database */
 $link = pg_connect("host=".DB_SERVER." dbname=". DB_NAME ." user=" . DB_USERNAME . " password=" .DB_PASSWORD. "")
